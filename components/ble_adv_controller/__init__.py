8c36ee932fb092c784678bc4b36fac27b5ded5b8
Fabian <77135162+NobisIndustries@users.noreply.github.com>
2026-01-20 19:32:05 +0100

    Fix another ESPHome incompatibility

--- components/ble_adv_controller/__init__.py
+++ components/ble_adv_controller/__init__.py
@@ -256,7 +256,7 @@ class BleAdvRegistry:
         if not cls.handler:
             hdl_id = ID("ble_adv_static_handler", type=BleAdvHandler)
             cls.handler = cg.new_Pvariable(hdl_id)
-            cg.add(cls.handler.set_component_source("ble_adv_handler"))
+            # Component source is now set automatically in ESPHome 2025.x
             cg.add(cg.App.register_component(cls.handler))
             for encoding, params in BLE_ADV_ENCODERS.items():
                 for variant, param_variant in params["variants"].items():
